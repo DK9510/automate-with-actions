c5b358671dc4eb719b9344995495462f505ac405
DIshant Kapadiya <60573506+DK9510@users.noreply.github.com>
2022-04-05 19:32:09 +0530

    installation script updated

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,37 +3,37 @@
 # this script is to install necessary tools in the vm
 
 echo "Start Installing...."
-#sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade
 
-# install go 1.16 version
-wget https://dl.google.com/go/go1.16.4.linux-amd64.tar.gz 
-sudo tar -xvf go1.16.4.linux-amd64.tar.gz 
+# install go 1.17 version
+wget https://dl.google.com/go/go1.17.8.linux-amd64.tar.gz 
+sudo tar -xvf go1.17.8.linux-amd64.tar.gz 
 sudo mv -f go /usr/local/
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go 
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 go version
 #install gobuster 
-go install github.com/OJ/gobuster/v3@latest
+go install -v github.com/OJ/gobuster/v3@latest
 # install  tools and depandency
-go get -v github.com/OWASP/Amass/v3/...
+go install -v github.com/OWASP/Amass/v3/...@master
 # amass
 sudo apt install -y libpcap-dev
-GO111MODULE=on go get -v github.com/projectdiscovery/naabu/v2/cmd/naabu
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 # naabu port scanner
-GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 # subFinder
-go get -u github.com/tomnomnom/assetfinder
+go install -v github.com/tomnomnom/assetfinder@latest
 # asset Finder
-GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 # Nuclei vuln scanner
-GO111MODULE=on go get -v github.com/projectdiscovery/notify/cmd/notify
+go install -v github.com/projectdiscovery/notify/cmd/notify@latest
 # notfy we need configuration file 
-go get -u github.com/tomnomnom/anew
+go install -v github.com/tomnomnom/anew@latest
 # anew
-GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 # httpx
-go get -u github.com/tomnomnom/httprobe
+go install -v github.com/tomnomnom/httprobe@latest
 #Httprobe
 
 #go path don't remove it
