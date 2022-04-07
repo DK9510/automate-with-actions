58b0c3e5a5bdf9ea2bb5a1756f1493b02497135d
DIshant Kapadiya <60573506+DK9510@users.noreply.github.com>
2022-04-07 22:24:42 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@
 
 echo "Start Installing...."
 sudo apt-get update && sudo apt-get upgrade
-sudo apt install golang-go 
+#sudo apt install golang-go 
 # install go 1.17 version
 # wget https://dl.google.com/go/go1.17.8.linux-amd64.tar.gz 
 # sudo tar -xvf go1.17.8.linux-amd64.tar.gz 
@@ -14,26 +14,26 @@ export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 go version
 #install gobuster 
-go install -v github.com/OJ/gobuster/v3@latest
+#go install -v github.com/OJ/gobuster/v3@latest
 # install  tools and depandency
 go install -v github.com/OWASP/Amass/v3/...@master
 # amass
-sudo apt install -y libpcap-dev
-go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+#sudo apt install -y libpcap-dev
+#go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 # naabu port scanner
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 # subFinder
 go install -v github.com/tomnomnom/assetfinder@latest
 # asset Finder
-go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+#go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 # Nuclei vuln scanner
 go install -v github.com/projectdiscovery/notify/cmd/notify@latest
 # notfy we need configuration file 
 go install -v github.com/tomnomnom/anew@latest
 # anew
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+#go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 # httpx
-go install -v github.com/tomnomnom/httprobe@latest
+#go install -v github.com/tomnomnom/httprobe@latest
 #Httprobe
 
 #go path don't remove it
